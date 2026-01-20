6a3c905784e8058d3b8ae4ce71d633a01a63416c
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2026-01-20 23:16:00 +0530

    make changes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,40 +1,36 @@
-const fs = require ("fs");
+const fs = require("fs");
 const cloudinary = require("../config/cloudinary.config");
-const upload = require ("../middlewares/upload.middleware");
-const  uniqueEmail = require("../middlewares/uniqueEmail.middleware");
-
-
+const upload = require("../middlewares/upload.middleware");
+const uniqueEmail = require("../middlewares/uniqueEmail.middleware");
 
 const _route = express.Router();
-_route.post("/signup",upload.single("profilepicture"))
+_route.post("/signup", upload.single("profilepicture"));
 
-const signup =async(req,res) =>{
-    try{
-        const {name,email}= req.body;
-        if(!req.file){
-            return res.status(400).json({
-                message:"profile picture is required"
-            })
-        }
-        const result = await cloudinary.uploader.upload(req.file,{
-            folder:"profile pictures"
-        });
-        fs.unlinkSync(req.file.path);
-        res.status(201).json({
-            message:"user registered successfully",
-            user:{
-                id,
-                name,
-                email,
-                profilePicture
-            }
-        })
+const signup = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    if (!req.file) {
+      return res.status(400).json({
+        message: "profile picture is required",
+      });
     }
-    catch(error){
-        res.status(500).json({
-            message:"signup failed",
-        });
-    }
-
-}
+    const result = await cloudinary.uploader.upload(req.file, {
+      folder: "profile pictures",
+    });
+    fs.unlinkSync(req.file.path);
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: "auto-generated-id",
+        name: "User Name",
+        email: "user@email.com",
+        profilePic: "cloudinary_image_url",
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "signup failed",
+    });
+  }
+};
 module.exports = signup;
